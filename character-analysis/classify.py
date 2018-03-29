5d95716fed952e036c6890be7d2fc32d7b7fcdd4
Hima <himat15@yahoo.com>
2018-03-29 02:10:12 -0400

    Initial file to classify chars given a single line (not fin)

---
+++ character-analysis/classify.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pickle
+
+indices_file = "chars_in_text_lines.pkl"
+
+
+def load_char_indices():
+    with open(indices_file, 'rb') as fh:
+       character_line_dict = pickle.load(fh)
+
+    return character_line_dict
+
+def classify_single_line():
+
+    character_line_dict = load_char_indices()
+
+    # k = ("Ace Buckley", 'h')
+
+    # print(len(character_line_dict[k]))
+    # print(character_line_dict[k])
+
+    print(len(character_line_dict))
+
+    for key in character_line_dict:
+        print(key)
+        print(character_line_dict[key])
+
+if __name__ == "__main__":
+    classify_single_line()
