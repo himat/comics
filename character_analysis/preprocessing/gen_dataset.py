2de543bc96b8ae756164fd6f508d153e5e55c4d3
Himakeerth Tammineedi htammine@andrew.cmu.edu <himat15@yahoo.com>
2018-04-22 01:34:15 -0400

    Fixed nan issue in generated data

--- character_analysis/preprocessing/gen_dataset.py
+++ character_analysis/preprocessing/gen_dataset.py
@@ -23,6 +23,9 @@ CHAR_TYPE = "char_type"
 CHAR_TYPE_HERO = "hero"
 CHAR_TYPE_VILLAIN = "villain"
 
+# How many each of positive and negative examples to generate
+num_examples = 9000
+
 # Each beginning line in the generated data needs to have at least this many 
 #   words so that we don't use lines where the character name appears right at 
 #   the beginning of the line, which wouldn't be possible to properly predict
@@ -30,8 +33,8 @@ min_num_words_in_line = 3
 
 # Generates a list of cloze sentences each with a random non-character word removed
 def gen_cloze_nonchar_data(comics_text, num_examples, heroes_all_names, villains_all_names):
-
     assert(len(comics_text) >= num_examples)
+    print("\n\nGenerating data with random non-character names removed")
 
     generated_nonchar_data = []
 
@@ -54,24 +57,28 @@ def gen_cloze_nonchar_data(comics_text, num_examples, heroes_all_names, villains
         # print(chosen_word_ind)
         chosen_word = words[chosen_word_ind]
 
+
         if len(chosen_word) < 2: # word should be at least 2 chars long
             continue
 
+        # Don't choose a word that's a hero/villain name
         if chosen_word in heroes_all_names.values or chosen_word in villains_all_names.values:
             continue
+
+        # TODO: make sure the chosen word isn't just punctuation
 
         # Get line up to the last occurrence of the word to ensure
         #   line is long enough
         chosen_word_start_pos = line.rfind(chosen_word)
         line_begin = line[:chosen_word_start_pos]
-        
+
         # print(f"{chosen_word} |||| {line}")
         # print("---", line_begin)
 
         generated_nonchar_data.append({TEXT: line_begin, 
                                        NEXT_WORD: chosen_word,
                                        IS_CHAR: False,
-                                       CHAR_TYPE: None})
+                                       CHAR_TYPE: "None"})
 
     if len(generated_nonchar_data) < num_examples:
         raise Exception("not enough examples in data which satisfy requirements")
@@ -91,23 +98,8 @@ def gen_cloze_char_sentence(line, name):
     # line_begin = line[:split_pos]
 
     return line_begin
-    
 
-# Loops through OCR file to build up dataset
-def create_dataset():
-
-    # Filter false so it doesn't replace empty strings with NaN
-    comics_df = pd.read_csv(comics_ocr_file, na_filter=False)
-    comics_text = comics_df.text
-    heroes_names = pd.read_csv(heroes_file).Name
-    heroes_unfiltered_names = pd.read_csv(heroes_unfiltered_file).Name
-    villains_names = pd.read_csv(villains_file).Name
-    villains_unfiltered_names = pd.read_csv(villains_unfiltered_file).Name
-
-    heroes_names = heroes_names.str.lower()
-    heroes_unfiltered_names = heroes_unfiltered_names.str.lower()
-    villains_names = villains_names.str.lower()
-    villains_unfiltered_names = villains_unfiltered_names.str.lower()
+def gen_cloze_char_data(comics_text, num_examples, heroes_names, villains_names):
 
     print("\n\nGenerating data with character names removed")
     generated_data_train = []
@@ -120,9 +112,9 @@ def create_dataset():
 
     print_mod = 1000
     for line in comics_text:
+        words = re.findall(r"\w+", line)
         
         for hero in heroes_names.values:
-            words = re.findall(r"\w+", line)
             if hero in words and words.index(hero) > min_num_words_in_line:
 
                 generated_line = gen_cloze_char_sentence(line, hero) 
@@ -144,7 +136,6 @@ def create_dataset():
                 curr_gen_data_ptr = flip_ptr(curr_gen_data_ptr)
 
         for villain in villains_names.values:
-            words = re.findall(r"\w+", line)
             if villain in words and words.index(villain) > min_num_words_in_line:
 
                 generated_line = gen_cloze_char_sentence(line, villain) 
@@ -157,22 +148,46 @@ def create_dataset():
                 curr_gen_data_ptr = flip_ptr(curr_gen_data_ptr)
 
 
-        data_limit = 9000
-        if len(generated_data_test) == data_limit:
-            print(f"Reached data limit of {data_limit}")
+        if len(generated_data_test) == num_examples:
+            print(f"Reached data limit of {num_examples}")
             break
         if len(generated_data_test) % print_mod == 0:
-            print(f"{len(generated_data_test)}/{data_limit} lines added")
+            print(f"{len(generated_data_test)}/{num_examples} lines added")
 
     assert(len(generated_data_train) == len(generated_data_test))
 
-    print("\n\nGenerating data with random non-character names removed")
-    len_char_data = len(generated_data_train)
-    nonchar_data = gen_cloze_nonchar_data(comics_text, 2*len_char_data, heroes_unfiltered_names, villains_unfiltered_names)
-    generated_data_train += nonchar_data[:len_char_data]
-    generated_data_test += nonchar_data[len_char_data:]
+    return generated_data_train, generated_data_test
 
-    print(f"Generated {len_char_data} examples each of positive and negative cloze sentences")
+
+# Loops through OCR file to build up dataset
+def create_dataset():
+
+    # Filter false so it doesn't replace empty strings with NaN
+    comics_df = pd.read_csv(comics_ocr_file, na_filter=False)
+    comics_text = comics_df.text
+    heroes_names = pd.read_csv(heroes_file).Name
+    heroes_unfiltered_names = pd.read_csv(heroes_unfiltered_file).Name
+    villains_names = pd.read_csv(villains_file).Name
+    villains_unfiltered_names = pd.read_csv(villains_unfiltered_file).Name
+
+    heroes_names = heroes_names.str.lower()
+    heroes_unfiltered_names = heroes_unfiltered_names.str.lower()
+    villains_names = villains_names.str.lower()
+    villains_unfiltered_names = villains_unfiltered_names.str.lower()
+
+    generated_data_train, generated_data_test = [], []
+
+    char_data_train, char_data_test = gen_cloze_char_data(comics_text, num_examples, heroes_names, villains_names)
+    assert(len(char_data_train) == num_examples and len(char_data_test) == num_examples)
+    generated_data_train += char_data_train
+    generated_data_test += char_data_test
+
+    # TODO: make gen_cloze_nonchar_data work the same way as for char, where it returns both train/test so you don't have to multiply num_examples by 2
+    nonchar_data = gen_cloze_nonchar_data(comics_text, 2*num_examples, heroes_unfiltered_names, villains_unfiltered_names)
+    generated_data_train += nonchar_data[:num_examples]
+    generated_data_test += nonchar_data[num_examples:]
+
+    print(f"Generated {len(nonchar_data)//2} examples each of positive and negative cloze sentences")
     assert(len(generated_data_train) == len(generated_data_test))
 
     generated_data_train_df = pd.DataFrame(generated_data_train)
@@ -182,12 +197,16 @@ def create_dataset():
 
 
 if __name__ == "__main__":
+
     train_dataset_df, test_dataset_df = create_dataset()
-    
-    if not train_dataset_df[train_dataset_df.text.isnull()].empty:
-        raise ValueException("Empty text was generated")
-    if not test_dataset_df[test_dataset_df.text.isnull()].empty:
-        raise ValueException("Empty text was generated")
+
+    if train_dataset_df.isnull().values.any():
+        print(train_dataset_df[train_dataset_df.isnull().any(axis=1)])
+        print(train_dataset_df.info())
+        raise ValueError("Nulls exist")
+    if test_dataset_df.isnull().values.any():
+        print(test_dataset_df[test_dataset_df.isnull().any(axis=1)])
+        raise ValueError("Nulls exist")
 
     train_dataset_df.to_csv(dataset_train_file, index=False, na_rep="None", columns=[IS_CHAR, CHAR_TYPE, NEXT_WORD, TEXT], quoting=csv.QUOTE_NONNUMERIC)
     test_dataset_df.to_csv(dataset_test_file, index=False, na_rep="None", columns=[IS_CHAR, CHAR_TYPE, NEXT_WORD, TEXT], quoting=csv.QUOTE_NONNUMERIC)
